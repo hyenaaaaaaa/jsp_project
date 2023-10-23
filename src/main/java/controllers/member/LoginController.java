479ad43bbe0a705abd64fc2ed9944f0ebe7e2684
urbanecho97@gmail.com <urbanecho97@gmail.com>
2023-10-23 16:36:39 +0900

    로그아웃

--- src/main/java/controllers/member/LoginController.java
+++ src/main/java/controllers/member/LoginController.java
@@ -3,6 +3,7 @@ package controllers.member;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,22 +16,38 @@ import static commons.ScriptUtil.alertError;
 import static commons.ScriptUtil.go;
 
 @WebServlet("/member/login")
-public class LoginContoller extends HttpServlet {
+public class LoginController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/member/login.jsp");
         rd.forward(req, resp);
+
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             LoginService service = ServiceManager.getInstance().loginService();
             service.login(req);
+
+            /*아이디 저장 처리 S*/
+            String userId = req.getParameter("userId");
+            String saveId = req.getParameter("saveId");
+            Cookie cookie = new Cookie("saveId", userId);
+            if (saveId == null) { //미 체크 - 쿠키 제거
+                cookie.setMaxAge(0);
+            } else { //체크 상태 - 쿠키 저장
+                cookie.setMaxAge(60 * 60 * 24 * 365);
+            }
+
+            resp.addCookie(cookie);
+            /*아이디 저장 처리 E*/
 
             go(resp, req.getContextPath() + "/", "parent");
         } catch (RuntimeException e) {
             alertError(resp, e);
         }
+
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,15 +5,19 @@
 <fmt:setBundle basename="messages.commons" />
 <c:url var="loginUrl" value="/member/login" />
 <c:url var="joinUrl" value="/member/join" />
-<c:url var="LogoutUrl" value="/member/logout" />
+<c:url var="logoutUrl" value="/member/logout" />
 <c:url var="mypageUrl" value="/mypage" />
 
 <layout:main title="메인..">
-    <c:if test="${sessionScope.member == null}"> <%-- 미로그인 상태--%>
+    <c:if test="${sessionScope.member == null}"> <%-- 미 로그인 상태 --%>
         <a href="${loginUrl}">로그인</a>
         <a href="${joinUrl}">회원가입</a>
     </c:if>
-    <c:if test="%{sessionScope.member != null}"> <%-- 로그인 상태 --%>
+    <c:if test="${sessionScope.member != null}"> <%-- 로그인 상태 --%>
+        <fmt:message key="LOGIN_MSG">
+            <fmt:param>${sessionScope.member.userNm}</fmt:param>
+            <fmt:param>${sessionScope.member.userId}</fmt:param>
+        </fmt:message>
         <a href="${logoutUrl}">로그아웃</a>
         <a href="${mypageUrl}">마이페이지</a>
     </c:if>
